d805af09a07241f0c39284b8870e92fc099149b1
andrei <acb54@student.le.ac.uk>
2023-04-29 07:11:01 +0100

    In a similar way we have users it would be good if we could have a concept of Student within the system. It would be really good if we could Create, Read Update Delete (CRUD) Students. We need to know the name, email, dob, course they are studying.

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -4,7 +4,7 @@
       <span class="hipster img-fluid rounded"></span>
     </div>
     <div class="col-md-9">
-      <h1 class="display-4" v-text="$t('home.title')">Welcome to the application!</h1>
+      <h1 class="display-4" v-text="$t('home.title')">Welcome, Java Hipster!</h1>
       <p class="lead" v-text="$t('home.subtitle')">This is your homepage</p>
 
       <div>
@@ -27,6 +27,44 @@
           >
         </div>
       </div>
+
+      <p v-text="$t('home.question')">If you have any question on JHipster:</p>
+
+      <ul>
+        <li>
+          <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer" v-text="$t('home.link.homepage')"
+            >JHipster homepage</a
+          >
+        </li>
+        <li>
+          <a
+            href="http://stackoverflow.com/tags/jhipster/info"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-text="$t('home.link.stackoverflow')"
+            >JHipster on Stack Overflow</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/jhipster/generator-jhipster/issues?state=open"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-text="$t('home.link.bugtracker')"
+            >JHipster bug tracker</a
+          >
+        </li>
+        <li>
+          <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer" v-text="$t('home.link.chat')"
+            >JHipster public chat room</a
+          >
+        </li>
+        <li>
+          <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer" v-text="$t('home.link.follow')"
+            >follow @jhipster on Twitter</a
+          >
+        </li>
+      </ul>
 
       <p>
         <span v-text="$t('home.like')">If you like JHipster, don't forget to give us a star on</span>
---
+++ src/main/webapp/app/entities/student/student-update.component.ts
@@ -0,0 +1,90 @@
+import { Component, Vue, Inject } from 'vue-property-decorator';
+
+import { IStudent, Student } from '@/shared/model/student.model';
+import StudentService from './student.service';
+
+const validations: any = {
+  student: {
+    name: {},
+    email: {},
+    dob: {},
+    course: {},
+  },
+};
+
+@Component({
+  validations,
+})
+export default class StudentUpdate extends Vue {
+  @Inject('studentService') private studentService: () => StudentService;
+  public student: IStudent = new Student();
+  public isSaving = false;
+  public currentLanguage = '';
+
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      if (to.params.studentId) {
+        vm.retrieveStudent(to.params.studentId);
+      }
+    });
+  }
+
+  created(): void {
+    this.currentLanguage = this.$store.getters.currentLanguage;
+    this.$store.watch(
+      () => this.$store.getters.currentLanguage,
+      () => {
+        this.currentLanguage = this.$store.getters.currentLanguage;
+      }
+    );
+  }
+
+  public save(): void {
+    this.isSaving = true;
+    if (this.student.id) {
+      this.studentService()
+        .update(this.student)
+        .then(param => {
+          this.isSaving = false;
+          this.$router.go(-1);
+          const message = this.$t('gradroleApp.student.updated', { param: param.id });
+          return this.$root.$bvToast.toast(message.toString(), {
+            toaster: 'b-toaster-top-center',
+            title: 'Info',
+            variant: 'info',
+            solid: true,
+            autoHideDelay: 5000,
+          });
+        });
+    } else {
+      this.studentService()
+        .create(this.student)
+        .then(param => {
+          this.isSaving = false;
+          this.$router.go(-1);
+          const message = this.$t('gradroleApp.student.created', { param: param.id });
+          this.$root.$bvToast.toast(message.toString(), {
+            toaster: 'b-toaster-top-center',
+            title: 'Success',
+            variant: 'success',
+            solid: true,
+            autoHideDelay: 5000,
+          });
+        });
+    }
+  }
+
+  public retrieveStudent(studentId): void {
+    this.studentService()
+      .find(studentId)
+      .then(res => {
+        this.student = res;
+      });
+  }
+
+  public previousState(): void {
+    this.$router.go(-1);
+  }
+
+  public initRelationships(): void {}
+}
